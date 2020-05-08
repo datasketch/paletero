91f15410fc9c1dac83f139d140aa34cf1c7da6ca
Juan Pablo Marin Diaz <jpmarindiaz@gmail.com>
2020-05-08 14:55:53 -0500

    renamed homodatum fun

--- R/map_colors.R
+++ R/map_colors.R
@@ -7,9 +7,9 @@ map_colors <- function(data, color_by = NULL, palette = NULL,
   f <- fringe(data)
   if(is.empty(color_by)){
     palette <- palette[1]
-    values <- rep(1,getFringeStats(f)$nrow)
+    values <- rep(1,fringe_stats(f)$nrow)
   }else{
-    values <- pullFringeColumn(f, color_by)
+    values <- fringe_column(f, color_by)
   }
   colors <- paletero(values, palette)
   if(preview){
--- tests/testthat/test-map_colors.R
+++ tests/testthat/test-map_colors.R
@@ -6,5 +6,16 @@ test_that("Test map colors", {
 
   map_colors(data, color_by = 1, palette = paleta("magma"))
 
+  data <- sample_data("Cat-Num")
+  paletero::map_colors(data, color_by = NULL,
+                       palette = paleta("Set1"), colors_df = NULL)
+
+  f <- fringe(data)
+  paletero::map_colors(f, color_by = NULL,
+                       palette = paleta("Set1"), colors_df = NULL)
+  paletero::map_colors(f, color_by = names(f$data)[1],
+                       palette = paleta("Set1"), colors_df = NULL)
+
+
 
 })
