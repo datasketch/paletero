e2ca4867aeaee74ef7c8568939f580e0ae326f6c
Juan Pablo Marin Diaz <jpmarindiaz@gmail.com>
2020-05-15 03:17:19 -0500

    added colorblind palettes and colorblind test function. Closes #1

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -14,7 +14,7 @@ pals_all <- bind_rows(
   .id = "fun") %>%
   mutate(length = ifelse(is.na(length), 100, length),
          palette_name = paste0(package,"::",palette)
-         ) %>%
+  ) %>%
   filter(length <= 100)
 
 # pals_all <- sample_n(pals_all, 10)
@@ -34,12 +34,12 @@ paletteer <- map(pals, function(x){
     # type = ifelse(x$type == "qualitative", "categorical", x$type),
     type = x$type,
     generator = ifelse(fun == "paletteer_c", "continuous",
-                  ifelse(fun == "paletteer_d", "discrete", "dynamic")),
+                       ifelse(fun == "paletteer_d", "discrete", "dynamic")),
     length = x$length,
     palette_name = x$palette_name,
     package = x$package,
     colors = as.character(do.call(fun, params))
-    )}) %>%
+  )}) %>%
   set_names(pals_all$palette_name) %>%
   bind_rows()
 
@@ -48,14 +48,59 @@ paletteer <- map(pals, function(x){
 dspal <- system.file("palettes","datasketch.yaml", package = "paletero")
 dspal <- yaml::read_yaml(dspal)$palette
 dspal <- unlist(dspal, recursive = FALSE)
+dspal <- lapply(dspal, function(x){
+  x$length <- length(x$colors)
+  x
+})
 
 dspals <- dspal %>% bind_rows() %>%
   mutate(package = "paletero",
-        palette_name = paste(package, name, sep = "::"),
-        colors = paste0(colors, "FF")
-         )
+         palette_name = paste(package, name, sep = "::"),
+         colors = paste0(colors, "FF")
+  )
 
-palettes <- bind_rows(paletteer, dspals)
+
+
+# Colorblind
+library(colorBlindness)
+
+avpals <- colorBlindness::availablePalette()
+displayAvailablePalette()
+avpals <- transpose(tribble(
+  ~name,~type,
+  "paletteMartin","qualitative",
+  "Blue2DarkOrange12Steps","diverging",
+  "Blue2DarkOrange18Steps","diverging",
+  "Blue2DarkRed12Steps","diverging",
+  "Blue2DarkRed18Steps","diverging",
+  "Blue2Gray8Steps","diverging",
+  "Blue2Green14Steps","diverging",
+  "Blue2Orange10Steps","sequential",
+  "Blue2Orange12Steps","sequential",
+  "Blue2Orange8Steps","sequential",
+  "Blue2OrangeRed14Steps","sequential",
+  "Brown2Blue10Steps","sequential",
+  "Brown2Blue12Steps","sequential",
+  "Green2Magenta16Steps","diverging",
+  "LightBlue2DarkBlue10Steps","sequential",
+  "LightBlue2DarkBlue7Steps","sequential",
+  "ModifiedSpectralScheme11Steps","sequential",
+  "PairedColor12Steps","qualitative",
+  "SteppedSequential5Steps","stepped"
+))
+
+
+colorblindPalettes <- lapply(avpals, function(x){
+  x$colors = get(x$name)
+  x$length = length(x$colors)
+  x$package = "colorBlindness"
+  x$palette_name = paste(x$package, x$name, sep = "::")
+  x
+}) %>% bind_rows()
+
+
+
+palettes <- bind_rows(paletteer, dspals, colorblindPalettes)
 
 usethis::use_data(palettes, internal = TRUE, overwrite = TRUE)
 
