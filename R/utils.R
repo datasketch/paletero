944b7957a8ae007e88b3444171a0843330bee54e
CamilaAchuri <camilaachury.4@gmail.com>
2023-05-11 14:12:49 -0500

    sum nchar to validate conditional

--- R/utils.R
+++ R/utils.R
@@ -32,7 +32,7 @@ match_replace <- function(v,dic, force = TRUE){
     return(y)
   else if (is.null(x) || is.na(x))
     return(y)
-  else if (class(x) == "character" && nchar(x) == 0)
+  else if (class(x) == "character" && sum(nchar(x), na.rm = T) == 0)
     return(y)
   else x
 }
